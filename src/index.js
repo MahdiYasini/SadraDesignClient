1bfad0f6f6374cae301175eedffc191f09de6b03
M.Yasini <yasinimahdi98@gmail.com>
2020-03-17 11:58:02 +0330

    change name folders and set up base redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+import thunk from 'thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SetUpBaseInfo from './store/reducers/setUpBaseInfo';
+
+const rootReducer = combineReducers({
+    baseInfo: SetUpBaseInfo
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(<Provider store = {store}><App /> </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
