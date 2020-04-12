6fbc3b66f685abb3b01c940abca9d191a4b9e00b
Mahdi Yasini <34657907+MahdiYasini@users.noreply.github.com>
2020-04-12 11:29:48 +0430

    add information. (NI)

--- src/components/BlogPosts/BlogPosts.js
+++ src/components/BlogPosts/BlogPosts.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 
+//! this is for testing in real, when we get data from server send post info from PostContainer.
 import image from '../../assets/images/showCaseContentFirst_First_Main.jpg'
 
 
